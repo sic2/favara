b590051c3b58c3ded7748e49d6898bb9d0de2d78
Gabriele Viglianisi <vigliag@gmail.com>
2017-08-09 15:26:33 +0200

    fixed sanitize method not declared as class method

--- models/event.rb
+++ models/event.rb
@@ -3,19 +3,24 @@
 class Event < ApplicationRecord
   belongs_to :source, optional: true
 
+  def self.sanitize_str(str)
+    str
+  end
+
   def self.from_fb_event(fb_event)
     event = find_or_initialize_by(uid: fb_event['id'])
 
-    event.name = sanitize(fb_event['name'])
-    event.content = sanitize(fb_event['description'])
+    event.name = sanitize_str(fb_event['name'])
+    event.content = sanitize_str(fb_event['description'])
     event.starts_at = fb_event['start_time']
     event.ends_at = fb_event['end_time']
     event.organiser = fb_event.dig('parent_group', 'name') || fb_event.dig('owner', 'name')
-    event.location_name = sanitize fb_event.dig('place', 'name')
+    event.location_name = sanitize_str fb_event.dig('place', 'name')
 
     location = fb_event.dig('place', 'location')
+
     if location
-      event.location = sanitize(location.to_json)
+      event.location = sanitize_str(location.to_json)
       event.coordinates = "#{location['latitude']}, #{location['longitude']}"
     end
 
